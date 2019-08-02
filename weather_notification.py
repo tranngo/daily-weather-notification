4ef3399f7a553c3bebc252c51739b6c6f057235d
Tran Ngo <tranngo@usc.edu>
2019-08-02 04:25:01 -0400

    Added print_welcome_text() and read_config() functions

--- weather_notification.py
+++ weather_notification.py
@@ -1,21 +1,33 @@
 import requests
+import json
 
-
-def get_weather(zipcode, countrycode):
+def get_weather(postalcode, countrycode):
     # Weather API endpoint (https://openweathermap.org)
     weather_endpoint = 'api.openweathermap.org/data/2.5/weather'
 
     # Parameter(s) needed to make the API call
     payload = {
-        'zip' = zipcode
+        # 'zip' = postalcode
     }
 
     response = requests.get(url)
 
 def read_config():
+    # config_information_map is a dictionary that stores all values within the config file
+    config_information_map = { }
 
+    # Read the config file and store data into config_information_map
+    with open('config.json') as config_file:
+        data = json.load(config_file)
+    config_information_map['weather_apikey'] = data['weather_apikey']
+    return config_information_map
 
-def main():
+def print_welcome_text():
+    # user_info is a dictionary that stores the user's country code and postal code
+    user_info = { }
+
+    # country_code_map is a dictionary that stores countries with their
+    # corresponding 2 character ISO 3166 code
     country_code_map = {
         'Afghanistan': 'AF',
         'Algeria': 'DZ',
@@ -130,20 +142,45 @@ def main():
         'Uganda': 'UG',
         'Ukraine': 'UA',
         'United Kingdom': 'GB',
-        'United Republic of Tanzania': 'TZ',
         'United States': 'US',
         'Uruguay': 'UY',
         'Uzbekistan': 'UZ',
         'Venezuela': 'VE',
         'Vietnam': 'VN',
-        'Western Sahara': 'EH',
         'Yemen': 'YE',
         'Zambia': 'ZM',
         'Zimbabwe': 'ZW'
     }
 
-    
-    weather_apikey =
+    # Putting the keys of the country_code_map dictionary into a list
+    country_list = list(country_code_map.keys())
+
+    print("Daily Weather Notification by Tran Ngo")
+    print("-----------------------------------------------")
+    # Ask for user's country and store the associated country code
+    country_input = input("Please enter your country (to see a list of supported countries, type \"help\"): ")
+    while country_input not in country_list and country_input != "help":
+        print("Invalid input.")
+        country_input = input("Please enter your country (to see a list of supported countries, type \"help\"): ")
+    if country_input == "help":
+        print("")
+        for country in country_list:
+            print(country)
+    else:
+        user_info['countrycode'] = country_code_map[country_input]
+    # Ask for user's postal code and store it
+    postalcode_input = input("Please enter your postal code: ")
+    user_info['postalcode'] = postalcode_input
+    return user_info
+
+
+def main():
+    # Read the config file and store it into a dictionary
+    config_information_map = read_config()
+
+    # Assign the values of the config file to variables for use
+    weather_apikey = config_information_map['weather_apikey']
+    print_welcome_text()
 
 main()
 
