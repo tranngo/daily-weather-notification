8777951f78d3885db6e4649545f1b4c55b788a61
Tran Ngo <tranngo@usc.edu>
2019-08-03 09:51:38 -0400

    Implemented get_weather() function

--- weather_notification.py
+++ weather_notification.py
@@ -1,23 +1,45 @@
 import requests
 import json
 
-def get_weather(weather_apikey, latitude, longitude):
+def get_weather(weather_apikey, latitude, longitude, unit):
+    # weather_info is a dictionary that stores weather summary, highest temperature, and lowest temperature
+    weather_info = {}
+
     # Weather API endpoint (https://darksky.net)
     weather_endpoint = 'https://api.darksky.net/forecast/' + weather_apikey + "/" + latitude + "," + longitude
 
     # Optional parameters
-    payload = {
-        'exclude=currently,minutely,daily'
-    }
+    if unit == "Fahrenheit":
+        payload = {
+            'exclude': 'currently,minutely,daily,alerts,flags'
+        }
+    elif unit == "Celsius":
+        payload = {
+            'exclude': 'currently,minutely,daily,alerts,flags',
+            'unit': 'si'
+        }
 
     response = requests.get(weather_endpoint, params=payload)
     data = json.loads(response.text)
 
+    weather_info['summary'] = data['hourly']['summary']
 
+    # temperatures is a list that stores all temperatures throughout the day; used to find max/min temperature
+    temperatures = []
+
+    for tempdata in data['hourly']['data']:
+        temperatures.append(data['hourly']['data']['temperature'])
+
+    max_temperature = max(temperatures)
+    min_temperature = min(temperatures)
+
+    weather_info['max'] = max_temperature
+    weather_info['min'] = min_temperature
+    return weather_info
 
 def read_config():
     # config_information_map is a dictionary that stores all values within the config file
-    config_information_map = { }
+    config_information_map = {}
 
     # Read the config file and store data into config_information_map
     with open('config.json') as config_file:
@@ -28,7 +50,7 @@ def read_config():
 
 def get_latitude_longitude(google_apikey, address):
     # latitude_longitude is a dictionary that stores the user's latitude and longitude
-    latitude_longitude = { }
+    latitude_longitude = {}
 
     # Google's Geocoding API endpoint
     geocoding_endpoint = 'https://maps.googleapis.com/maps/api/geocode/json'
@@ -48,7 +70,7 @@ def get_latitude_longitude(google_apikey, address):
 
 def get_user_info():
     # user_info is a dictionary that stores the user's address and phone number
-    user_info = { }
+    user_info = {}
 
     # Ask for user's country and store the associated country code
     print("To get the most accurate temperature readings, enter your address.")
@@ -59,6 +81,15 @@ def get_user_info():
     print("Please use the following format: (123) 345-5678")
     phone_input = input("\t> ")
     user_info['phone'] = phone_input
+    print("The default unit is Fahrenheit. Would you like to switch to Celsius? (y/n)")
+    unit_input = input("\t> ")
+    while unit_input.lower() != "y" and unit_input.lower() != "n":
+        print("Invalid input.")
+        unit_input = "\t> "
+    if unit_input == "n":
+        user_info['unit'] = "Fahrenheit"
+    elif unit_input == "y":
+        user_info['unit'] = "Celsius"
 
     return user_info
 
